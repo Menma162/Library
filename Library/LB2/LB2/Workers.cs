c56c95428b44b6af6e1840f5e14356cdba3eb9d8
Irene <81463820+volar-a-la-nada@users.noreply.github.com>
2021-09-23 13:25:42 +0300

    Add files via upload

---
+++ Library/LB2/LB2/Workers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LB2
+{
+    //Сотрудник
+    class Workers
+    {
+        public string Name { get; set; }
+        public string Birthday { get; set; }
+        public string Post { get; set; }
+
+        /// <summary>
+        /// Добавление сотрудника
+        /// </summary>
+        /// <param name="Name">Имя сотрудника</param>
+        /// <param name="Birthday">День рождения</param>
+        /// <param name="Post">Должность</param>
+        public void Add(string Name, string Birthday, string Post)
+        {
+            this.Name = Name;
+            this.Birthday = Birthday;
+            this.Post = Post;
+        }
+
+        public Workers(string Name, string Birthday, string Post)
+        {
+            Add(Name, Birthday, Post);
+        }
+
+    }
+}
