2f04a83b4ffd6dceb11ecb86f1a16bf349a548f6
Menma162 <91255224+Menma162@users.noreply.github.com>
2021-09-23 13:25:44 +0300

    Add files via upload

--- Library/LB2/LB2/Workers.cs
+++ Library/LB2/LB2/Workers.cs
@@ -8,21 +8,5 @@ namespace LB2
 {
     class Workers
     {
-        public string Name { get; set; }
-        public string Birthday { get; set; }
-        public string Post { get; set; }
-
-        public void Add(string Name, string Birthday, string Post)
-        {
-            this.Name = Name;
-            this.Birthday = Birthday;
-            this.Post = Post;
-        }
-
-        public Workers(string Name, string Birthday, string Post)
-        {
-            Add(Name, Birthday, Post);
-        }
-
     }
 }
