bfcb44f6c93cdc6f88f93ac2ee160abe51778a65
Irene <81463820+volar-a-la-nada@users.noreply.github.com>
2021-09-23 13:20:56 +0300

    Add files via upload

--- Library/LB2/LB2/Workers.cs
+++ Library/LB2/LB2/Workers.cs
@@ -8,5 +8,21 @@ namespace LB2
 {
     class Workers
     {
+        public string Name { get; set; }
+        public string Birthday { get; set; }
+        public string Post { get; set; }
+
+        public void Add(string Name, string Birthday, string Post)
+        {
+            this.Name = Name;
+            this.Birthday = Birthday;
+            this.Post = Post;
+        }
+
+        public Workers(string Name, string Birthday, string Post)
+        {
+            Add(Name, Birthday, Post);
+        }
+
     }
 }
