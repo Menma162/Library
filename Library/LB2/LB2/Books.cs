346c22c16ae000d7c9668a8c9b38d493ad8347d6
Menma162 <91255224+Menma162@users.noreply.github.com>
2021-09-23 13:29:51 +0300

    Add files via upload

--- Library/LB2/LB2/Books.cs
+++ Library/LB2/LB2/Books.cs
@@ -8,6 +8,20 @@ namespace LB2
 {
     class Books
     {
-        public string name;
+        public string name { get; set; } //название книги
+        public string author { get; set; } //автор книги
+        public int pages { get; set; } // число страниц
+        
+        public Books(string Name, string Author, int Pages)
+        {
+            Add(Name, Author, Pages);
+        }
+
+        public void Add(string Name, string Author, int Pages)
+        {
+            name = Name;
+            author = Author;
+            pages = Pages;
+        }
     }
 }
